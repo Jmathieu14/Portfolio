fee6fa162a6ab1df85205bb97e8bb4a75a177459
Jacques Mathieu <jmathfb@gmail.com>
2019-10-07 16:45:17 -0400

    Remove link to example html page
    Update expanded resume;
    Switch home to prod version

--- pre-prod/js/layouts/home.js
+++ pre-prod/js/layouts/home.js
@@ -92,14 +92,6 @@ const pageLayout = {
                     "hoverBG": "#33ff5f",
                     "hoverBGName": "goodGreen",
                     "target": "_blank"
-                },
-                {
-                    "name": "Example Work",
-                    "url": "../html/example-work.html",
-                    "logo": "../img/page/example icon.svg",
-                    "hoverBG": "#3dffb9",
-                    "hoverBGName": "turqoise",
-                    "target": "_self"
                 }
             ],
             "opacityAsTab": myOpacityStyle
