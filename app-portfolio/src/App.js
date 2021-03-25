3369d96c7ec2d8133034eaa3ec99a344350e98f7
Jacques <jmathfb@gmail.com>
2021-03-25 12:10:33 -0400

    breaking changes

--- app-portfolio/src/App.js
+++ app-portfolio/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 import Home from './Home';
+import ExampleWork from './ExampleWork';
+import { NativeRouter, Route } from '../node_modules/react-native-router/index';
+import { View } from '../node_modules/react-native/index';
 
 function App() {
   return (
-    <Home></Home>
+    <NativeRouter>
+      <View>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/example-work' component={ExampleWork}></Route>
+      </View>
+    </NativeRouter>
   );
 }
 
