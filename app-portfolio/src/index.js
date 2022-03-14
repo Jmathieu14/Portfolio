851d31843130683a01640f276ef207c63cb08c94
Jacques Mathieu <jmathfb@gmail.com>
2022-03-14 17:23:12 -0400

    Add react-snap; add build folder to commits

--- app-portfolio/src/index.js
+++ app-portfolio/src/index.js
@@ -1,17 +1,23 @@
 import 'jquery';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import './assets/css/overhaul.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+const root = document.getElementById('root');
+
+const rootJSX =
   <React.StrictMode>
     <App />
     <span id="MODAL_RENDER_TARGET"></span>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>;
+
+if (root.hasChildNodes()) {
+  hydrate(rootJSX, root);
+} else {
+  render(rootJSX, root);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
